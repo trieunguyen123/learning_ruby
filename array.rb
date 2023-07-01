91cb6025ff60f97a16848ffdcf47419ca5b219e3
trieunguyen123 <43953455+trieunguyen123@users.noreply.github.com>
2023-07-01 17:04:54 +0700

    update array
    update lần 1

---
+++ array.rb
@@ -0,0 +1,68 @@
+arr = [1,2,3,4,5,6,7,8,9]
+
+
+#Hàm xuất ra số lẽ
+# def sole (arr)
+#     arr.select {|n| n.odd?}
+# end
+# puts "so le #{sole(arr)}"
+
+#hàm xuất ra số chẵn
+# def sochan(arr)
+#     arr.select {|n| n.even?}
+# end
+# puts "so chan #{sochan(arr)}"
+
+#hàm xuất ra số lớn nhất
+# def solonnhat (arr)
+#     arr.max 
+# end
+# puts "solonnhat #{solonnhat(arr)}"
+
+#hàm xuất ra số nhỏ nhất
+# def sonhonhat(arr)
+#     arr.min
+# end 
+# puts "sonhonhat #{sonhonhat(arr)}"
+
+#hàm xuất ra màn hình mãng giảm dần
+# def giamdan(arr)
+#     arr.sort {|a, b| b <=> a }
+# end
+# puts "giam dan #{giamdan(arr)}"
+
+#hàm xuất phép tính cộng 2 số bất kỳ trong mãng
+# def tongranrom(arr)
+#     a = arr.sample
+#     b = arr.sample
+#     puts " #{a} + #{b}  =  #{a +b}"
+# end
+#     puts tongranrom(arr)
+
+#hàm xuất phép tính trừ 2 số bất kỳ trong mãng mà kết quả luôn dương
+# def truranrom(arr)
+#     a = arr.sample
+#     b = arr.sample
+#     if a > b
+#         puts "#{a} - #{b} = #{a - b}"
+#     else
+#         puts "#{b} - #{a} = #{b - a}"
+#     end
+# end
+# truranrom(arr)
+
+#hàm xuất tổng các số trong mãng
+# def tongmang(arr)
+#     c = arr.sum
+# end
+# puts "tong mang la #{tongmang(arr)}"
+
+#hàm xuất số nguyên tố trong 
+# require 'prime' #import thư viện "prime"
+# def songuyento(arr)
+#     songuyento = arr.select{|n| Prime.prime?(n)}
+# end
+# puts "songuyento #{songuyento(arr)}"
+
+    
+
