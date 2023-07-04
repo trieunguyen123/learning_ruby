5f43832d410eb9975f3c8b708170c6a8b79e24a0
trieunguyen123 <43953455+trieunguyen123@users.noreply.github.com>
2023-07-04 17:52:45 +0700

    update hash and string

--- hash.rb
+++ hash.rb
@@ -1,46 +1,75 @@
-students = {
-    Ten: "Toan",
-    Tuoi: "20",
-    Diem: "5.5"
-}
-
-#thêm địa chỉ vào hash
-students[:DiaChi] = "123 Đường số 2"
-
-#lấy 1 phần tử trong hash
-puts students[:Diem]
-
-#lấy giá trị keys
-puts students.keys
-
-#lấy giá trị values
-puts students.values
-
-#delete 1 phần tử trong hash
-students.delete(:Diem)
-puts "#{students}"
-
-#lấy key và value có trong hash 
-students.each do |students| # có thể thay students  = |key| và |value|
-    puts "#{students}"
-end
+# students = {
+#     Ten: "Toan",
+#     Tuoi: "20",
+#     Diem: "5.5"
+# }
+#
+# #thêm địa chỉ vào hash
+# students[:DiaChi] = "123 Đường số 2"
+#
+# #lấy 1 phần tử trong hash
+# puts students[:Diem]
+#
+# #lấy giá trị keys
+# puts students.keys
+#
+# #lấy giá trị values
+# puts students.values
+#
+# #delete 1 phần tử trong hash
+# students.delete(:Diem)
+# puts "#{students}"
+#
+# #lấy key và value có trong hash
+# students.each do |students| # có thể thay students  = |key| và |value|
+#     puts "#{students}"
+# end
 
 #Kiễm tra Key và value có trong hash
-puts students.has_key?(:Diem)
-puts students.has_value?("20")
+# puts students.has_key?(:Diem)
+# puts students.has_value?("20")
+#
+# #Merge thêm 1 hash vào hash hiện có
+# thongTinThem = {SoThich:"Doc Sach" , HocLuc:"Kha"}
+# tatcathongtin = students.merge(thongTinThem)
+# puts "#{tatcathongtin}"
+#
+# #Đếm số lần xuất hiện từ trong câu
+# def dem_tu (str)
+#     tu = str.downcase.split(" ")
+#     soLanLap = Hash.new(0)
+#     tu.each {|tu| soLanLap[tu] +=1 }
+#     return soLanLap
+# end
+# puts dem_tu("Hom Nay An Gi? Hom Nay Toi An Bun Rieu")
 
-#Merge thêm 1 hash vào hash hiện có
-thongTinThem = {SoThich:"Doc Sach" , HocLuc:"Kha"}
-tatcathongtin = students.merge(thongTinThem)
-puts "#{tatcathongtin}"
-
-#Đếm số lần xuất hiện từ trong câu
-def dem_tu (str)
-    tu = str.downcase.split(" ")
-    soLanLap = Hash.new(0)
-    tu.each {|tu| soLanLap[tu] +=1 } 
-    return soLanLap
+#tạo 1 hash class lưu học sinh và số lượng học sinh
+def input_students()
+    n_students = 0
+    while n_students <= 0
+        puts "Nhập Số Lượng Học Sinh"
+        n_students = gets.chomp.to_i
+    end
+    students = {}
+    n_students.times do |i|
+        puts "Nhập học sinh thứ #{i+1}"
+        puts "Tên"
+        name = gets.chomp
+        puts "Điểm"
+        scores = gets.chomp.to_f
+        puts "Học Lực"
+        degree = gets.chomp
+        students[name] = {scores: scores ,degree: degree }
+    end
+    class_info = {number_studen: n_students, students: students}
+    return class_info
 end
-puts dem_tu("Hom Nay An Gi? Hom Nay Toi An Bun Rieu")
-
-
+class_info = input_students()
+students = class_info[:students]
+puts"Danh Sách Học Sinh"
+students.each do |name, info|
+  puts "Tên: #{name}"
+  puts "Điểm: #{info[:scores]}"
+  puts "Học Lực: #{info[:degree]}"
+end
+puts students
---
+++ string.rb
@@ -0,0 +1,50 @@
+# Đếm độ dài chữ or câu dùng length
+# def lengt_of_string ()
+# puts "Nhập Chữ or Câu"
+# str = gets.chomp
+# puts "Độ dài của từ or câu = #{str.length}"
+# end
+# lengt_of_string()
+
+#nhập 2 chuỗi so sánh độ dài của nhau dùng length (length và size như nhau)
+# def compare_string()
+#   puts "nhập chuỗi 1"
+#   str1= gets.chomp
+#   puts "nhập chuỗi 2"
+#   str2 = gets.chomp
+#   if str1.length > str2.length
+#     puts "chuỗi 1 dài hơn chuỗi 2"
+#   elsif str1.length < str2.length
+#     puts "chuỗi 1 ngắn hơn chuỗi 2"
+#   else
+#     puts "chuỗi 1 bằng chuỗi 2"
+#   end
+# end
+# compare_string()
+
+#Nhập 2 chuỗi và nối chuỗi với nhau
+# def connect_string()
+#   puts "nhập chuỗi 1"
+#   str1 = gets.chomp
+#   puts "nhập chuỗi 2"
+#   str2 = gets.chomp
+#   connect_str = str1 << " " << str2
+#   puts "chuỗi nối lại : #{connect_str}"
+# end
+# connect_string()
+
+#tách chuỗi
+# def split_string()
+#   puts "Nhập chuỗi"
+#   str = gets.chomp
+#   arr = str.split (" ")
+#   count = 0
+#   arr.each do |s|
+#     puts "'#{s}'"
+#     count += 1
+#   end
+#   puts "số lượng chuỗi đã tách : #{count}"
+# end
+# split_string()
+
+
