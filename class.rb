8a70cc967922dcf2327d0afddd7362cf8bcd4785
trieunguyen123 <43953455+trieunguyen123@users.noreply.github.com>
2023-07-06 18:29:30 +0700

    update class.rb

--- class.rb
+++ class.rb
@@ -43,3 +43,38 @@
 # rectangle =Rectangle.create
 # puts rectangle.area
 
+# hiển thị thông tin người dùng
+class Person
+  attr_accessor :name , :age , :address
+  def initialize (name, age , address)
+    @name = name
+    @age = age
+    @address = address
+  end
+  def show_person
+    puts "Hi #{@name}"
+  end
+  def check_age
+    age = @age.to_i
+    if age < 18
+      puts "chưa vị thành niên"
+    else
+      puts "vị thành niên"
+    end
+  end
+  def change_address(new_address)
+    @address = new_address
+  end
+end
+class Person1 < Person # kế thùa từ lớp Person
+  def show_full_info
+    puts "#{@name} #{@age} #{@address}"
+  end
+end
+person1 = Person.new("trieu","23","asd123")
+puts person1.show_person
+person2 = Person1.new("trieu","23","asd123")
+puts person2.show_full_info
+puts person2.show_person
+
+
